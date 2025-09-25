b88d2881f4c0b78cbf946c145d3e298b7c0d5a07
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-25 18:27:23 -0500

    Workin' whit Queues

---
+++ Queues/Queues.UI.Console/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using QueueArray;
+using System.ComponentModel.Design;
+
+var queue = new QueueUsingArray<int>(5);   //5 - Queue Capacity
+var opt = string.Empty;
+do
+{
+    opt = Menu();
+    switch (opt)
+    {
+        case "1":
+            try 
+            {
+                Console.Write("Enter a number: ");
+                var num = int.Parse(Console.ReadLine()!);
+                queue.Enqueue(num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+        case "2":
+            try 
+            {
+                var item = queue.Dequeue();
+                Console.WriteLine($"Item dequeue: {item}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+        case "3":
+            try 
+            {
+                var item = queue.Peek();
+                Console.WriteLine($"Item at front: {item}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+    }
+} while (opt != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Enqueue");
+    Console.WriteLine("2. Dequeue");
+    Console.WriteLine("3. Peek");
+    Console.WriteLine("0. Exit");
+    Console.Write("Select an option: ");
+    return Console.ReadLine()!;
+}
+
+
+
+
